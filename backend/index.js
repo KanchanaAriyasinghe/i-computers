aaca4bbc293c5a31a69a0eae87a7690e9edd048e
KanchanaAriyasinghe <kanchana2000idunil@gmail.com>
2026-05-23 23:20:26 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,11 @@ mongoose.connect(mongodbURI).then(
         console.log("connected to mongoDB")
     }
 )
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json())
 // call middleware 
 app.use (authenticateUser)
